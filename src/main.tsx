f81af17165be6c73da0a7b65a152208bce593a7f
Ian Huang (Shaoru) <ian1314159@gmail.com>
2024-03-18 02:23:36 -0700

    feat: implement offline mode so i can work while wifi is down

--- src/main.tsx
+++ src/main.tsx
@@ -9,4 +9,16 @@ import 'ace-builds/src-noconflict/theme-clouds_midnight';
 import 'ace-builds/src-noconflict/keybinding-vim';
 import 'ace-builds/src-noconflict/ext-elastic_tabstops_lite';
 
+import { registerSW } from 'virtual:pwa-register';
+
+// add this to prompt for a refresh
+const updateSW = registerSW({
+  onNeedRefresh() {
+    updateSW(true);
+  },
+  onOfflineReady() {
+    console.log('Offline ready!');
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        globPatterns: ['**/*'],
+      },
+      includeAssets: ['**/*'],
+      manifest: {
+        theme_color: 'white',
+        background_color: 'white',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        short_name: 'Type Here | App',
+        description: 'Type here...',
+        name: 'Type Here | App',
+        icons: [
+          {
+            src: '/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
